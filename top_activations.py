cfcd7fb05bbdc4251b867804ca5b6d443e729c78
shehper <ali.shehper1@gmail.com>
2024-01-07 22:16:09 -0500

    cleaned up top_activations a bit more

--- top_activations.py
+++ top_activations.py
@@ -1,5 +1,5 @@
 """
-Load a trained autoencoder model and compute its top activations
+Load a trained autoencoder model to compute its top activations
 
 Run on a macbook on a Shakespeare dataset as 
 python top_activations.py --dataset=shakespeare_char --gpt_dir=out-shakespeare-char --device=cpu --eval_contexts=1000 --autoencoder_subdir=1704386782.22-autoencoder-shakespeare_char
@@ -9,7 +9,6 @@ import torch
 import os
 from train_sae import AutoEncoder, get_text_batch
 import numpy as np
-import gc
 from model import GPTConfig, GPT
 import pickle # needed to load meta.pkl
 import tiktoken # needed to decode contexts to text
@@ -20,7 +19,7 @@ device = 'cuda' # change it to cpu
 seed = 1442
 dataset = 'openwebtext' 
 gpt_dir = 'out' 
-gpt_batch_size = 16 # batch size for computing reconstruction nll 
+eval_batch_size = 16 # batch size for computing reconstruction nll 
 eval_contexts = 10000 # 10 million in anthropic paper; but we can choose 1 million as OWT dataset is smaller
 tokens_per_eval_context = 10 # same as anthropic paper
 autoencoder_dir = 'out_autoencoder' # directory containing weights of various trained autoencoder models
@@ -28,25 +27,26 @@ autoencoder_subdir = '' # subdirectory containing the specific model to consider
 length_context_on_each_side = 4 # number of tokens to print/save on either side of the token with top feature activation
 k = 5 # number of top activations
 
-slice_fn = lambda storage: storage[iter * gpt_batch_size: (iter + 1) * gpt_batch_size]
+slice_fn = lambda storage: storage[iter * eval_batch_size: (iter + 1) * eval_batch_size]
 
 def top_activations_and_tokens(f_subset, token_indices, contexts, context_on_each_side, k):
-    """ computes top feature activations for each feature and the corresponding tokens (with context around them)
-    input: f_subset of shape (gpt_batch_size, tokens_per_eval_context, n_features)
-              token_indices of shape  (gpt_batch_size, tokens_per_eval_context))
-              contexts of shape (gpt_batch_size, block_size)
+    """ 
+        computes top feature activations for each feature and the corresponding tokens (with context around them)
+    input: f_subset of shape (eval_batch_size, tokens_per_eval_context, n_features)
+              token_indices of shape  (eval_batch_size, tokens_per_eval_context))
+              contexts of shape (eval_batch_size, block_size)
               context_on_each_side: an int, must satisfy (2 * context_on_each_side + 1) < block_size
-              k: an int, the number of top activation values to compute and return, must be < gpt_batch_size * tokens_per_eval_context
+              k: an int, the number of top activation values to compute and return, must be < eval_batch_size * tokens_per_eval_context
     returns: 
               top_values: a tensor of shape (k, n_features)
               top_tokens_with_context: a tensor of shape (k, n_features, 2 * context_on_each_side + 1)
               """
 
-    assert f_subset.shape == (gpt_batch_size, tokens_per_eval_context, n_features)
-    assert token_indices.shape == (gpt_batch_size, tokens_per_eval_context)
-    assert contexts.shape == (gpt_batch_size, block_size)
+    assert f_subset.shape == (eval_batch_size, tokens_per_eval_context, n_features)
+    assert token_indices.shape == (eval_batch_size, tokens_per_eval_context)
+    assert contexts.shape == (eval_batch_size, block_size)
     assert (2 * context_on_each_side + 1) < block_size, "this code will give errors if length_context_on_each_side is too long compared to block_size"
-    assert k < gpt_batch_size * tokens_per_eval_context, "number of top values to choose cannot be bigger than the number of tokens in each batch"
+    assert k < eval_batch_size * tokens_per_eval_context, "number of top values to choose cannot be bigger than the number of tokens in each batch"
 
     flattened_f = f_subset.view(-1, f_subset.shape[-1]) # (m * n, p)
     top_values, flattened_indices = torch.topk(flattened_f, k=k, dim=0) # (k, p) 
@@ -61,6 +61,17 @@ def top_activations_and_tokens(f_subset, token_indices, contexts, context_on_eac
     return top_values, top_tokens_with_contexts
 
 def special_decode(tokens):
+    """
+    Decodes a list of tokens into a string. This function has the special feature of 
+    highlighting the middle token in red, which is useful for identifying the token 
+    with the highest feature activation.
+
+    Input:
+        tokens: List[str]. A list of tokens to decode.
+
+    Output:
+        str: The decoded string, with the middle token wrapped in red color.
+    """
     assert len(tokens) % 2 == 1, "tokens must have odd length so that the middle token is unique"
 
     left_context = decode(tokens[:len(tokens)//2])
@@ -96,7 +107,6 @@ if __name__ == '__main__':
     autoencoder = AutoEncoder(n_ffwd, n_features, lam=l1_coeff).to(device)
     autoencoder.load_state_dict(state_dict)
 
-
     ## load tokenized text data
     data_dir = os.path.join('data', config['dataset'])
     text_data = np.memmap(os.path.join(data_dir, 'train.bin'), dtype=np.uint16, mode='r')
@@ -119,7 +129,6 @@ if __name__ == '__main__':
     if compile:
         gpt = torch.compile(gpt) # requires PyTorch 2.0 (optional)
     config['block_size'] = block_size = gpt.config.block_size
-
     
     ## load tokenizer used to train the gpt model
     # look for the meta pickle in case it is available in the dataset folder
@@ -141,18 +150,14 @@ if __name__ == '__main__':
         encode = lambda s: enc.encode(s, allowed_special={"<|endoftext|>"})
         decode = lambda l: enc.decode(l)
 
-
-
     ## Get text data for evaluation 
     X, Y = get_text_batch(text_data, block_size=block_size, batch_size=eval_contexts) # (eval_contexts, block_size) 
 
-    ## Compute and store MLP activations, full transformer loss and ablated MLP loss on evaluation text data
+    # Compute and store MLP activations on evaluation text data
     mlp_activations_storage = torch.tensor([], dtype=torch.float16)
-    # residual_stream_storage = torch.tensor([], dtype=torch.float16)
-    # full_loss, mlp_ablated_loss = 0, 0
     
     ## start evaluation
-    num_eval_batches = eval_contexts // gpt_batch_size
+    num_eval_batches = eval_contexts // eval_batch_size
     for iter in range(num_eval_batches):    
         
         print(f'iter = {iter}/{num_eval_batches} in computation of mlp_acts for eval data')
@@ -163,11 +168,6 @@ if __name__ == '__main__':
 
         _, mlp_activations, batch_loss, batch_ablated_loss = gpt.forward_with_and_without_mlp(x, y)    
         mlp_activations_storage = torch.cat([mlp_activations_storage, mlp_activations.to(dtype=torch.float16, device='cpu')])
-        # residual_stream_storage = torch.cat([residual_stream_storage, res_stream.to(dtype=torch.float16, device='cpu')])
-        # full_loss += batch_loss
-        # mlp_ablated_loss += batch_ablated_loss
-
-    # full_loss, mlp_ablated_loss = full_loss/num_eval_batches, mlp_ablated_loss/num_eval_batches
 
     token_indices = torch.randint(length_context_on_each_side, block_size - length_context_on_each_side, (eval_contexts, tokens_per_eval_context)) # (eval_contexts, tokens_per_eval_context)
     # TODO: This assumes that the tokens chosen for studying top activations are not on the extreme ends of any text in the context window of gpt
@@ -178,45 +178,34 @@ if __name__ == '__main__':
     # feature_activation_counts = torch.zeros(n_features, dtype=torch.float32) # initiate with zeros
 
     for iter in range(num_eval_batches):
-
         print(f'{iter}/{num_eval_batches} for evaluation')
 
         # select batch of mlp activations, residual stream and y 
         if device_type == 'cuda':
-            # batch_targets = slice_fn(Y).pin_memory().to(device, non_blocking=True) 
             batch_mlp_activations = slice_fn(mlp_activations_storage).pin_memory().to(device, non_blocking=True) 
-            # batch_res_stream = slice_fn(residual_stream_storage).pin_memory().to(device, non_blocking=True) 
         else:
-            # batch_targets = slice_fn(Y).to(device) # (gpt_batch_size, block_size)
-            batch_mlp_activations = slice_fn(mlp_activations_storage).to(device) # (gpt_batch_size, block_size, n_ffwd)
-            # batch_res_stream = slice_fn(residual_stream_storage).to(device) # (gpt_batch_size, block_size, n_embd)
+            batch_mlp_activations = slice_fn(mlp_activations_storage).to(device) # (eval_batch_size, block_size, n_ffwd)
 
-        
         with torch.no_grad():
             batch_loss, batch_f, batch_reconstructed_activations, batch_mseloss, batch_l1loss = autoencoder(batch_mlp_activations)
         
-        batch_f = batch_f.to('cpu') # (gpt_batch_size, block_size, n_features)
-        batch_token_indices = slice_fn(token_indices) # (gpt_batch_size, tokens_per_eval_context)
+        batch_f = batch_f.to('cpu') # (eval_batch_size, block_size, n_features)
+        batch_token_indices = slice_fn(token_indices) # (eval_batch_size, tokens_per_eval_context)
         batch_contexts = slice_fn(X)
 
         # restrict batch_contexts and batch_f on the subset of tokens specified by batch_token_indices
-        batch_contexts_subset = torch.gather(batch_contexts, 1, batch_token_indices) # (gpt_batch_size, tokens_per_eval_context)
-        batch_f_subset = torch.gather(batch_f, 1, batch_token_indices.unsqueeze(-1).expand(-1, -1, n_features)) # (gpt_batch_size, tokens_per_eval_context, n_features)
-
-        # for each feature, calculate the TOTAL number of tokens on which it is active; shape: (n_features, ) 
-        # feature_activation_counts += torch.count_nonzero(batch_f_subset, dim=[0, 1]) # (n_features, )
-
+        batch_contexts_subset = torch.gather(batch_contexts, 1, batch_token_indices) # (eval_batch_size, tokens_per_eval_context)
+        batch_f_subset = torch.gather(batch_f, 1, batch_token_indices.unsqueeze(-1).expand(-1, -1, n_features)) # (eval_batch_size, tokens_per_eval_context, n_features)
+        
+        # compute top feature activation values and contexts in which they appear
         batch_top_values, batch_top_contexts = top_activations_and_tokens(batch_f_subset, batch_token_indices, batch_contexts, length_context_on_each_side, k)
         for feature in range(n_features):
             for top_index in range(k):
                 top_dict[feature].append((batch_top_values[top_index, feature].item(), special_decode(batch_top_contexts[top_index, feature].tolist())))
-            #print(top_dict)
             top_dict[feature] = sorted(top_dict[feature], reverse=True)[:k] # keep only the top k values overall
-
 
     # save the dictionary of top activations and tokens in the same directory and sub-directory as the autoencoder
     top_dict_path = os.path.join(autoencoder_dir, autoencoder_subdir, 'top_dict.pkl')
-
     with open(top_dict_path, 'wb') as fp:
         pickle.dump(top_dict, fp)
         print(f'top_dict saved successfully to file {top_dict_path}')
