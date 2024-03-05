bf107ccfed618216b06dc071309a7dde25a12470
shehper <ali.shehper1@gmail.com>
2024-03-04 20:46:04 -0500

    major changes to top_activations. started working on improving this file

--- autoencoder/top_activations.py
+++ autoencoder/top_activations.py
@@ -2,44 +2,76 @@
 Load a trained autoencoder model to compute its top activations
 
 Run on a macbook on a Shakespeare dataset as 
-python top_activations.py --device=cpu --dataset=shakespeare_char --gpt_dir=out-shakespeare-char --device=cpu --eval_contexts=1000 --autoencoder_subdir=1704914564.90-autoencoder-shakespeare_char
+python top_activations.py --device=cpu --dataset=shakespeare_char --gpt_dir=out-shakespeare-char --device=cpu --num_contexts=1000 --autoencoder_subdir=1704914564.90-autoencoder-shakespeare_char
 """
 
-import torch 
+import torch
+from tensordict import TensorDict 
 import os
 import numpy as np
 import pickle # needed to load meta.pkl
 import tiktoken # needed to decode contexts to text
 import sys 
 from autoencoder import AutoEncoder
-from train import get_text_batch
 
 ## Add path to the transformer subdirectory as it contains GPT class in model.py
 sys.path.insert(0, '../transformer')
 from model import GPTConfig, GPT
 
-# hyperparameters --- same as train_sae.py except a few maybe # TODO: do I need all of them?
+# hyperparameters 
 device = 'cuda' # change it to cpu
 seed = 1442
 dataset = 'openwebtext' 
 gpt_dir = 'out' 
-eval_batch_size = 16 # batch size for computing reconstruction nll 
-eval_contexts = 10000 # 10 million in anthropic paper; but we can choose 1 million as OWT dataset is smaller
-tokens_per_eval_context = 10 # same as anthropic paper
 autoencoder_dir = 'out_autoencoder' # directory containing weights of various trained autoencoder models
 autoencoder_subdir = '' # subdirectory containing the specific model to consider
-context_length = 4 # number of tokens to print/save on either side of the token with feature activation. 
-k = 20 # number of top activations
+eval_batch_size = 156 # batch size for computing reconstruction nll # TODO: this should have a different name. # B
+num_contexts = 10000 # 10 million in anthropic paper; but we will choose the entire dataset as our dataset is small # N
+eval_tokens = 10 # same as Anthropic's paper; number of tokens in each context on which feature activations will be computed # M
+num_tokens_either_side = 4 # number of tokens to print/save on either side of the token with feature activation. 
+# let 2 * num_tokens_either_side + 1 be denoted by W.
+k = 10 # number of top activations for each feature; 20 in Anthropic's visualization
 num_intervals = 11 # number of intervals to divide activations in; = 11 in Anthropic's work
 interval_exs = 5 # number of examples to sample from each interval of activations 
 modes_density_cutoff = 1e-3 # TODO: remove this; it is not being used anymor
 publish_html = False
 make_histogram = False
-k = 15 # number of top activations
-num_intervals = 11 # number of intervals to divide activations in; = 11 in Anthropic's work
-interval_exs = 5 # number of examples to sample from each interval of activations 
 
-slice_fn = lambda storage: storage[iter * eval_batch_size: (iter + 1) * eval_batch_size]
+slice_fn = lambda data: data[iter * eval_batch_size: (iter + 1) * eval_batch_size]
+
+def sample_tokens(*args, fn_seed=0):
+    # given tensors each of shape (B, T, ...), return tensors on randomly selected tokens
+    # and windows around them. shape of output: (B, M, W, ...)
+    
+    assert args and isinstance(args[0], torch.Tensor), "must provide at least one torch tensor as input"
+    assert args[0].ndim >=2, "input tensor must at least have 2 dimensions"
+    B, T = args[0].shape[:2]
+    for tensor in args[1:]:
+        assert tensor.shape[:2] == (B, T), "all tensors in input must have the same shape along the first two dimensions"
+
+    torch.manual_seed(fn_seed)
+    V = num_tokens_either_side # W = 2 * V + 1
+    M = eval_tokens
+    # select indices for tokens
+    token_idx_BM = torch.randint(V, T - V, (B, M))
+    # include windows
+    window_idx_BMW = token_idx_BM.unsqueeze(-1) + torch.arange(-V, V + 1)
+    # obtain batch indices
+    batch_indices_BMW = torch.arange(B).view(-1, 1, 1).expand_as(window_idx_BMW)
+    # print(args[1][batch_indices_BMW, window_idx_BMW, :])
+
+    result_tensors = []
+    for tensor in args:
+        if tensor.ndim == 3:  # For (B, T, F) tensors
+            sliced_tensor = tensor[batch_indices_BMW, window_idx_BMW, :]
+        elif tensor.ndim == 2:  # For (B, T) tensors
+            sliced_tensor = tensor[batch_indices_BMW, window_idx_BMW]
+        else:
+            raise ValueError("Tensor dimensions not supported. Only 2D and 3D tensors are allowed.")
+        result_tensors.append(sliced_tensor)
+
+    return result_tensors
+
 
 if __name__ == '__main__':
 
@@ -60,7 +92,7 @@ if __name__ == '__main__':
     autoencoder_ckpt_path = os.path.join(autoencoder_dir, autoencoder_subdir, 'ckpt.pt')
     autoencoder_ckpt = torch.load(autoencoder_ckpt_path, map_location=device)
     state_dict = autoencoder_ckpt['autoencoder']
-    n_features, n_ffwd = state_dict['enc.weight'].shape
+    n_features, n_ffwd = state_dict['enc.weight'].shape # H, F
     l1_coeff = autoencoder_ckpt['config']['l1_coeff']
     autoencoder = AutoEncoder(n_ffwd, n_features, lam=l1_coeff).to(device)
     autoencoder.load_state_dict(state_dict)
@@ -86,60 +118,88 @@ if __name__ == '__main__':
     gpt.to(device)
     if compile:
         gpt = torch.compile(gpt) # requires PyTorch 2.0 (optional)
-    config['block_size'] = block_size = gpt.config.block_size
+    config['block_size'] = block_size = gpt.config.block_size # T
 
-    ## Get text data for evaluation 
-    X, Y = get_text_batch(text_data, block_size=block_size, batch_size=eval_contexts) # (eval_contexts, block_size) 
+    ## select X, Y from text data
+    # as openwebtext and shakespeare_char are small datasets, use the entire text data. split it into len(text_data)//block_size contexts
+    if dataset in ["openwebtext", "shakespeare_char"]:
+        X_NT = torch.stack([torch.from_numpy((text_data[i:i+block_size]).astype(np.int64)) for i in range(len(text_data)//block_size)])
+        # TODO: note that we don't need y_BT/y_NT unless we compute feature ablations
+        # Y_NT = torch.stack([torch.from_numpy((text_data[i+1:i+1+block_size]).astype(np.int64)) for i in range(len(text_data)//block_size)])
+        num_contexts = X_NT.shape[0] # overwrite num_contexts = N
+    else:
+        raise NotImplementedError("""if the text dataset is too large, such as The Pile, you may not evaluate on the whole dataset.
+                                    In this case, use get_text_batch function to randomly select num_contexts contexts""") # TODO
 
-    # Compute and store MLP activations on evaluation text data
-    mlp_activations_storage = torch.tensor([], dtype=torch.float16) # TODO: Maybe this should be pre-defined as zeros
-    
-    ## start evaluation
-    num_eval_batches = eval_contexts // eval_batch_size
-    for iter in range(num_eval_batches):    
+    ## compute and store MLP activations 
+    data_NMW = TensorDict({
+        "mlp_acts": torch.empty(num_contexts, eval_tokens, 2 * num_tokens_either_side + 1, n_ffwd),
+        "tokens": torch.empty(num_contexts, eval_tokens, 2 * num_tokens_either_side + 1),
+        "feature_acts": torch.empty(num_contexts, eval_tokens, 2 * num_tokens_either_side + 1),
+    }, batch_size=[num_contexts, eval_tokens, 2 * num_tokens_either_side + 1]
+    )
+    # TODO: only process one feature at a time. There will be a trade-off (at least for now) 
+    # between how many features to process versus
+    # how much data to use because of memory and storage constraints. 
+
+    # mlp_acts_NMWF = torch.empty(num_contexts, eval_tokens, 2 * num_tokens_either_side + 1, n_ffwd)
+    num_batches = num_contexts // eval_batch_size
+    print(f"We will compute MLP activations in {num_batches} batches")
+    for iter in range(num_batches):    
+        print(f"Computing MLP activations for batch # {iter+1}/{num_batches}")
         
-        print(f'iter = {iter}/{num_eval_batches-1} in computation of mlp_acts for eval data')
-
-        # pin arrays x,y, which allows us to move them to GPU asynchronously (non_blocking=True)
-        x = slice_fn(X).pin_memory().to(device, non_blocking=True) if device_type == 'cuda' else slice_fn(X).to(device)
-        y = slice_fn(Y).pin_memory().to(device, non_blocking=True) if device_type == 'cuda' else slice_fn(Y).to(device)
-
-        _, mlp_activations, batch_loss, batch_ablated_loss = gpt.forward_with_and_without_mlp(x, y)    
-        mlp_activations_storage = torch.cat([mlp_activations_storage, mlp_activations.to(dtype=torch.float16, device='cpu')])
-
-    # sample indices of tokens from each context where feature activations will be computed.
-    token_indices = torch.randint(context_length, block_size - context_length, (eval_contexts, tokens_per_eval_context)) # (eval_contexts, tokens_per_eval_context)
-    # TODO: This assumes that the tokens chosen for studying top activations are not on the extreme ends of any text in the context window of gpt
-    # Can the generality we lose this way significantly affect our analysis? Put another way, could there be some interesting behavior on the edges of the context window?
-
-    # The logic should be as follows:
-    # For each context we should compute feature activations as in batch_f below
-    # Restrict to token_indices #TODO: is this really necessary or should we consider all tokens in each context? What difference does this make conceptually?
-    # Keep a dictionary feature_infos = {i: [] for i in range(n_features)]
-    # Whenever a feature activation is non-zero, add it to the list as (feature_activation, token with context around it)
-    # At the very end, sample 
-    # TODO: I think one thing that I should also try to keep track of is the location of the start of each context in train.bin
-    # Why? Well, If I intend to compute correlation between contexts of top activations, this might be needed. 
-    # Otherwise, I could just compute it from my list feature_infos[i].
-    # By the way, I should look at MMCS/cosine similarity in the paper. They might already be doing what I am intending to do. 
-
-    feature_infos = {i: [] for i in range(n_features)}
-
-    for iter in range(num_eval_batches):
-        print(f'{iter}/{num_eval_batches-1} for evaluation')
-
-        # select batch of mlp activations, residual stream and y 
-        if device_type == 'cuda':
-            batch_mlp_activations = slice_fn(mlp_activations_storage).pin_memory().to(device, non_blocking=True) 
-        else:
-            batch_mlp_activations = slice_fn(mlp_activations_storage).to(device) # (eval_batch_size, block_size, n_ffwd)
-
-        with torch.no_grad():
-            output = autoencoder(batch_mlp_activations)
+        # select text input for the batch
+        x_BT = slice_fn(X_NT).pin_memory().to(device, non_blocking=True) if device_type == 'cuda' else slice_fn(X_NT).to(device)
         
-        batch_f = output['f'].to('cpu') # (eval_batch_size, block_size, n_features)
-        batch_token_indices = slice_fn(token_indices) # (eval_batch_size, tokens_per_eval_context)
-        batch_contexts = slice_fn(X) # (eval_batch_size, block_size)
+        # compute MLP activations 
+        batch_mlp_acts_BTF = gpt.get_mlp_acts(x_BT) # (B, T, F) # TODO: Learn to use hooks instead? 
+        
+        # sample tokens and store MLP activations only for the sampled tokens and windows around them
+        batch_mlp_acts_BMWF, batch_tokens_BMW = sample_tokens(batch_mlp_acts_BTF, x_BT, fn_seed=seed+iter)
+        data_NMW["mlp_acts"][iter * eval_batch_size: (iter + 1) * eval_batch_size] = batch_mlp_acts_BMWF.to(device='cpu')
+        data_NMW["tokens"][iter * eval_batch_size: (iter + 1) * eval_batch_size] = batch_tokens_BMW
+        
+    print(data_NMW["tokens"][-75:-70])
+    print(data_NMW["mlp_acts"][-75:-70])
+    print(num_contexts)
+    print(torch.count_nonzero(data_NMW["tokens"]))
+    # TODO: can't figure out why some of the tokens stay zero. hmm. 
+    # TODO: also figure out what changes to make in order to make this work with cuda. 
+    raise 
+
+    # TODO: p should probably be called W instead as it is the length of the window
+    # TODO: n should be called M -- this is the number of tokens per eval context that we will be computing 
+
+#    feature_infos = {i: [] for i in range(n_features)}
+    # TODO: compute the largest batch size for which I should be able to evaluate
+
+    # for each feature, we want at the end, 
+    # a tensor of k windows
+    # for each window, we want all token ids and feature activation values
+    # so (k, W, 2) 
+    # and a tensor of shape
+    # (num_intervals, interval_exs, W, 2) 
+    # These could be obtained from 
+    # 
+    print(f"Now we will compute feature activations")
+    for feature_id in range(n_features):
+        feature_activations = torch.tensor() # TODO: what should be the shape?
+        # num_contexts, eval_tokens, 2 * num_tokens_on_each_side + 1
+        for iter in range(num_batches):
+            print(f"Computing feature activations for feature # {feature_id} in batch # {iter+1}/{num_batches}")
+
+            # select batch of mlp activations
+            if device_type == 'cuda':
+                batch_mlp_activations_BTpF = slice_fn(mlp_acts_NtpF).pin_memory().to(device, non_blocking=True) 
+            else:
+                batch_mlp_activations_BTpF = slice_fn(mlp_acts_NtpF).to(device) # (B, T, p, F)
+
+            with torch.no_grad():
+                output = autoencoder(batch_mlp_activations)
+            
+            batch_f = output['f'].to('cpu') # (eval_batch_size, block_size, n_features)
+            batch_token_indices = slice_fn(token_indices) # (eval_batch_size, tokens_per_eval_context)
+            batch_contexts = slice_fn(X) # (eval_batch_size, block_size)
 
         # TODO: this is picking different subset of tokens for different features. 
         # Instead replace the outer for loop with 
@@ -162,9 +222,6 @@ if __name__ == '__main__':
                         context_acts = [(s, t) for s, t in zip(context, f_acts)]
                         feature_infos[i] += [(curr_f_subset[k, m].item(), context_acts)]
 
-    #with open(os.path.join(autoencoder_dir, autoencoder_subdir, 'feature_infos.pkl'), 'wb') as f:
-    #    pickle.dump(feature_infos, f)
-    #print(f'saved feature_infos.pkl in {os.path.join(autoencoder_dir, autoencoder_subdir)}')
 
     if publish_html:
         ## load tokenizer used to train the gpt model
