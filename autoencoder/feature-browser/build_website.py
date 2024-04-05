4db302c593bd62c82cc80beb44f2afa0782fcadb
shehper <ali.shehper1@gmail.com>
2024-04-05 16:10:03 -0400

    committing changes in build_website at an intermediate state again

--- autoencoder/feature-browser/build_website.py
+++ autoencoder/feature-browser/build_website.py
@@ -7,7 +7,7 @@ S: num_sampled_tokens
 R: window_radius
 W: window_length
 L: number of autoencoder latents
-N: num_contexts
+N: total_sampled_tokens = (num_contexts * num_sampled_tokens)
 T: block_size (same as nanoGPT)
 B: gpt_batch_size (same as nanoGPT)
 SI: samples_per_interval
@@ -43,7 +43,7 @@ autoencoder_subdir = 0.0 # subdirectory containing the specific model to conside
 num_contexts = 10000 
 eval_batch_size = 156 # batch size for computing reconstruction nll # TODO: call it gpt_batch_size 
 # feature page hyperparameter
-num_sampled_tokens = 10 # number of tokens in each context on which feature activations will be computed # S
+num_sampled_tokens = 10 # number of tokens in each context on which feature activations will be computed 
 window_radius = 4 # number of tokens to print on either side of a sampled token.. # V / R
 num_top_activations = 10 # number of top activations for each feature 
 num_intervals = 12 # number of intervals to divide activations in; = 12 in Anthropic's work
@@ -77,13 +77,12 @@ encode, decode = resourceloader.load_tokenizer()
 n_features, n_ffwd = autoencoder.encoder.weight.shape
 html_out = os.path.join(os.path.dirname(os.path.abspath('.')), 'out', str(autoencoder_subdir))
 
-X, _ = resourceloader.get_text_batch(num_contexts=num_contexts)
+X, _ = resourceloader.get_text_batch(num_contexts=num_contexts) # (num_contexts, T)
 
 ## glossary of variables
-S = num_sampled_tokens
-V = window_radius
-M = num_contexts * S
-W = 2 * V + 1 # window length
+total_sampled_tokens = num_contexts * num_sampled_tokens
+window_length = 2 * window_radius + 1
+W = 2 * window_radius + 1 # window length
 I = num_intervals
 B = eval_batch_size
 
@@ -96,17 +95,22 @@ n_phases = n_features // n_features_per_phase + (n_features % n_features_per_pha
 n_batches = num_contexts // B + (num_contexts % B != 0)
 print(f"Will process features in {n_phases} phases. Each phase will have forward pass in {n_batches} batches")
 
+
 for phase in range(n_phases): 
     H = n_features_per_phase if phase < n_phases - 1 else n_features - (phase * n_features_per_phase)
+    print(f'working on phase # {phase + 1}/{n_phases}: features # {phase * n_features_per_phase} through {phase * n_features_per_phase + H}')   
     # TODO: the calculation of H could probably be made better. Am I counting 1 extra? What about the case when 
     # n_features % n_features_per_phase == 0
-    print(f'working on phase # {phase + 1}/{n_phases}: features # {phase * n_features_per_phase} through {phase * n_features_per_phase + H}')   
+    # data, H, top_acts_data_kWH
+    
+
+    # data = compute_feature_activations
     ## compute and store feature activations # TODO: data_MW should be renamed to something more clear. 
-    data_MW = TensorDict({
-        "tokens": torch.zeros(M, W, dtype=torch.int32),
-        "feature_acts_H": torch.zeros(M, W, H),
-        }, batch_size=[M, W]
-        )
+    data = TensorDict({
+        "tokens": torch.zeros(total_sampled_tokens, window_length, dtype=torch.int32),
+        "feature_acts": torch.zeros(total_sampled_tokens, window_length, H),
+        }, batch_size=[total_sampled_tokens, window_length]
+        ) # (N, W)
 
     for iter in range(n_batches): 
         print(f"Computing feature activations for batch # {iter+1}/{n_batches} in phase # {phase + 1}/{n_phases}")
@@ -120,24 +124,24 @@ for phase in range(n_phases):
         feature_acts_BTH = autoencoder.get_feature_activations(inputs=mlp_acts_BTF, 
                                                                 start_idx=phase*H, 
                                                                 end_idx=(phase+1)*H)
-        # sample tokens from the context, and save feature activations and tokens for these tokens in data_MW.
+        # sample tokens from the context, and save feature activations and tokens for these tokens in data.
         X_PW, feature_acts_PWH = select_context_windows(x, feature_acts_BTH, 
                                                         num_sampled_tokens=num_sampled_tokens, 
                                                         window_radius=window_radius, 
                                                         fn_seed=seed+iter) # P = B * S
-        data_MW["tokens"][iter * B * num_sampled_tokens: (iter + 1) * B * num_sampled_tokens] = X_PW 
-        data_MW["feature_acts_H"][iter * B * num_sampled_tokens: (iter + 1) * B * num_sampled_tokens] = feature_acts_PWH
+        data["tokens"][iter * B * num_sampled_tokens: (iter + 1) * B * num_sampled_tokens] = X_PW  # (N, W)
+        data["feature_acts"][iter * B * num_sampled_tokens: (iter + 1) * B * num_sampled_tokens] = feature_acts_PWH # (N, W, L)
 
         del mlp_acts_BTF, feature_acts_BTH, x, X_PW, feature_acts_PWH; gc.collect(); torch.cuda.empty_cache() 
 
     ## Get top k feature activations
     print(f'computing top k feature activations in phase # {phase + 1}/{n_phases}')
-    _, topk_indices_kH = torch.topk(data_MW["feature_acts_H"][:, window_radius, :], k=num_top_activations, dim=0)
+    _, topk_indices_kH = torch.topk(data["feature_acts"][:, window_radius, :], k=num_top_activations, dim=0) # (k, L)
     # evaluate text windows and feature activations to topk_indices_kH to get texts and activations for top activations
     top_acts_data_kWH = TensorDict({
-        "tokens": data_MW["tokens"][topk_indices_kH].transpose(dim0=1, dim1=2),
-        "feature_acts": torch.stack([data_MW["feature_acts_H"][topk_indices_kH[:, i], :, i] for i in range(H)], dim=-1)
-        }, batch_size=[num_top_activations, W, H])
+        "tokens": data["tokens"][topk_indices_kH].transpose(dim0=1, dim1=2), # (k, W, L)
+        "feature_acts": torch.stack([data["feature_acts"][topk_indices_kH[:, i], :, i] for i in range(H)], dim=-1)
+        }, batch_size=[num_top_activations, W, H]) # (k, W, L)
         
     # TODO: is my definition of ultralow density neurons consistent with Anthropic's definition?
     # TODO: make sure there are no bugs in switch back and forth between feature id and h.
@@ -152,7 +156,7 @@ for phase in range(n_phases):
         
         feature_id = phase * n_features_per_phase + h
         ## check whether feature is alive, dead or ultralow density based on activations on sampled tokens
-        curr_feature_acts_MW = data_MW["feature_acts_H"][:, :, h]
+        curr_feature_acts_MW = data["feature_acts"][:, :, h]
         mid_token_feature_acts_M = curr_feature_acts_MW[:, window_radius]
         num_nonzero_acts = torch.count_nonzero(mid_token_feature_acts_M)
 
@@ -162,7 +166,7 @@ for phase in range(n_phases):
             continue 
 
         ## make a histogram of non-zero activations
-        act_density = torch.count_nonzero(curr_feature_acts_MW) / (M * W) * 100
+        act_density = torch.count_nonzero(curr_feature_acts_MW) / (total_sampled_tokens * W) * 100
         non_zero_acts = curr_feature_acts_MW[curr_feature_acts_MW !=0]
         make_histogram(activations=non_zero_acts, 
                         density=act_density, 
@@ -184,7 +188,7 @@ for phase in range(n_phases):
                                         for j in range(I)], dim=0) # (I, SI)
         original_indices = sorted_indices_M[sampled_indices] # (I, SI)
         sampled_acts_data_IXW = TensorDict({
-            "tokens": data_MW["tokens"][original_indices],
+            "tokens": data["tokens"][original_indices],
             "feature_acts": curr_feature_acts_MW[original_indices],
             }, batch_size=[I, samples_per_interval, W]) # (I, SI, W)
 
@@ -195,5 +199,5 @@ for phase in range(n_phases):
                                     sampled_acts_data = sampled_acts_data_IXW,
                                     dirpath=html_out)
 
-    if phase == 1:
+    if phase == 0:
         break
