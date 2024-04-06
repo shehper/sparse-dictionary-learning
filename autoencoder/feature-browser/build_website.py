8adfc4e1aceab6fac1a4f9c3803f70ae28d6e1f1
shehper <ali.shehper1@gmail.com>
2024-04-06 02:42:06 -0400

    committing FeatureBrowser class again at an intermediate state

--- autoencoder/feature-browser/build_website.py
+++ autoencoder/feature-browser/build_website.py
@@ -13,20 +13,16 @@ B: gpt_batch_size (same as nanoGPT)
 SI: samples_per_interval
 
 Run on a Macbook as
-python build_website.py --device=cpu --dataset=shakespeare_char --gpt_ckpt_dir=out_sc_1_2_32 --autoencoder_subdir=1712254759.95
+python build_website.py --device=cpu --dataset=shakespeare_char --gpt_ckpt_dir=out_sc_1_2_32 --sae_ckpt_dir=1712254759.95
 """
 
+from dataclasses import dataclass
 import torch
 from tensordict import TensorDict 
 import os
 import sys 
 from main_page import create_main_html_page
 from subpages import write_alive_feature_page, write_dead_feature_page, write_ultralow_density_feature_page
-
-## Add path to the transformer subdirectory as it contains GPT class in model.py
-sys.path.insert(0, '../../transformer')
-from model import GPTConfig
-from hooked_model import HookedGPT
 
 sys.path.insert(1, '../')
 from resource_loader import ResourceLoader
@@ -36,17 +32,17 @@ from utils.plotting_utils import make_histogram
 # data and model
 dataset = 'openwebtext' 
 gpt_ckpt_dir = 'out' 
-autoencoder_subdir = 0.0 # subdirectory containing the specific model to consider
-# evaluation hyperparameters
-num_contexts = 10000 
-gpt_batch_size = 156 # batch size for computing reconstruction nll 
+sae_ckpt_dir = 0.0 # subdirectory containing the specific model to consider
 # feature page hyperparameter
+num_contexts = 10000
 num_sampled_tokens = 10 # number of tokens in each context on which feature activations will be computed 
 window_radius = 4 # number of tokens to print on either side of a sampled token.. # V / R
 num_top_activations = 10 # number of top activations for each feature 
 num_intervals = 12 # number of intervals to divide activations in; = 12 in Anthropic's work
 samples_per_interval = 5 # number of examples to sample from each interval of activations 
-n_features_per_phase = 20 # due to memory constraints, it's useful to process features in phases.
+# evaluation hyperparameters
+gpt_batch_size = 156 
+num_features_per_phase = 20 # due to memory constraints, it's useful to process features in phases.
 # system
 device = 'cuda' # change it to cpu
 # reproducibility
@@ -58,47 +54,65 @@ exec(open('../configurator.py').read()) # overrides from command line or config 
 config = {k: globals()[k] for k in config_keys} # will be useful for logging
 # -----------------------------------------------------------------------------
 
-# TODO: should configurator be moved to after __name__ == __main__? Will it be executed if 
-# the class is imported in another file?
-# Also, we don't need config here as we are not logging anything. 
+@dataclass
+class FeatureBrowserConfig:
+    # dataset and model
+    dataset: str = "openwebtext"
+    gpt_ckpt_dir: str = "out" 
+    sae_ckpt_dir: str = "out"
+    # feature browser hyperparameters
+    num_contexts: int = int(1e6)
+    num_sampled_tokens: int = 10
+    window_radius: int = 4
+    num_top_activations: int = 10
+    num_intervals: int = 12
+    samples_per_interval: int = 5
+    # processing hyperparameters
+    seed: int = 0
+    device: str = "cpu"
+    gpt_batch_size: int = 156
+    num_features_per_phase: int = 20
+    
 class FeatureBrowser(ResourceLoader):
-    def __init__(self, dataset, gpt_ckpt_dir, device, autoencoder_subdir, num_contexts, num_sampled_tokens, 
-                 window_radius, eval_batch_size, num_top_activations, num_intervals, samples_per_interval, seed):
+    def __init__(self, config):
         super().__init__(
-            dataset=dataset, 
-            gpt_ckpt_dir=gpt_ckpt_dir,
-            device=device,
+            dataset=config.dataset, 
+            gpt_ckpt_dir=config.gpt_ckpt_dir,
+            device=config.device,
             mode="eval",
-            sae_ckpt_dir=str(autoencoder_subdir),
+            sae_ckpt_dir=str(config.sae_ckpt_dir),
         )
  
-        self.encode, self.decode = self.load_tokenizer()
-        self.n_features, self.n_ffwd = self.autoencoder.encoder.weight.shape
-        self.html_out = os.path.join(os.path.dirname(os.path.abspath('.')), 'out', self.dataset, str(autoencoder_subdir))        
-        self.num_contexts = num_contexts
-        self.num_sampled_tokens = num_sampled_tokens
-        self.window_radius = window_radius
-        self.eval_batch_size = eval_batch_size
+        # retrieve feature browser hyperparameters from config
+        self.num_contexts = config.num_contexts
+        self.num_sampled_tokens = config.num_sampled_tokens
+        self.window_radius = config.window_radius
         self.num_top_activations = num_top_activations
         self.num_intervals = num_intervals
         self.samples_per_interval = samples_per_interval
-        self.seed = seed
-
+        self.window_length = 2 * self.window_radius + 1
         self.total_sampled_tokens = self.num_contexts * self.num_sampled_tokens  # Define total sampled tokens
 
-        self.n_features_per_phase = 20  # Adjust based on memory constraints
-        self.n_phases = self.n_features // self.n_features_per_phase + (self.n_features % self.n_features_per_phase != 0)
-        self.n_batches = self.num_contexts // self.eval_batch_size + (self.num_contexts % self.eval_batch_size != 0)
-        print(f"Will process features in {self.n_phases} phases. Each phase will have forward pass in {self.n_batches} batches")
+        self.gpt_batch_size = config.gpt_batch_size
+        self.num_features_per_phase = config.num_features_per_phase
+        self.n_features = self.autoencoder.n_latents
+        self.num_phases = self.n_features // self.num_features_per_phase + (self.n_features % self.num_features_per_phase != 0)
+        self.num_batches = self.num_contexts // self.gpt_batch_size + (self.num_contexts % self.gpt_batch_size != 0)
+        
+        self.encode, self.decode = self.load_tokenizer()
+        self.html_out = os.path.join(os.path.dirname(os.path.abspath('.')), 'out', config.dataset, str(config.sae_ckpt_dir))        
+        self.seed = config.seed 
+
+        print(f"Will process features in {self.num_phases} phases. Each phase will have forward pass in {self.num_batches} batches")
 
     def build(self):
         create_main_html_page(n_features=self.n_features, dirpath=self.html_out)
         X, _ = self.get_text_batch(num_contexts=self.num_contexts)
-        
-        for phase in range(self.n_phases):
+
+        for phase in range(self.num_phases):
             H = self.calculate_H(phase) # H needs to get a better name
-            print(f'working on phase # {phase + 1}/{self.n_phases}: \
-                  features # {phase * self.n_features_per_phase} through {phase * self.n_features_per_phase + H}')
+            print(f'working on phase # {phase + 1}/{self.num_phases}: \
+                  features # {phase * self.num_features_per_phase} through {phase * self.num_features_per_phase + H}')
             data = self.compute_feature_activations(phase, H, X)
             self.process_phase(data, H, phase)
 
@@ -107,46 +121,46 @@ class FeatureBrowser(ResourceLoader):
                 break
 
     def calculate_H(self, phase):
-        if phase < self.n_phases - 1:
-            return self.n_features_per_phase
+        if phase < self.num_phases - 1:
+            return self.num_features_per_phase
         else:
-            return self.n_features - (phase * self.n_features_per_phase)
+            return self.n_features - (phase * self.num_features_per_phase)
 
     def compute_feature_activations(self, phase, H, X):
         data = TensorDict({
-            "tokens": torch.zeros(self.total_sampled_tokens, 2 * self.window_radius + 1, dtype=torch.int32),
-            "feature_acts": torch.zeros(self.total_sampled_tokens, 2 * self.window_radius + 1, H),
-        }, batch_size=[self.total_sampled_tokens, 2 * self.window_radius + 1])
+            "tokens": torch.zeros(self.total_sampled_tokens, self.window_length, dtype=torch.int32),
+            "feature_acts": torch.zeros(self.total_sampled_tokens, self.window_length, H),
+        }, batch_size=[self.total_sampled_tokens, self.window_length]) # (N*S, W)
 
-        for iter in range(self.n_batches):
-            print(f"Computing feature activations for batch # {iter+1}/{self.n_batches} in phase # {phase + 1}")
-            start_idx = iter * self.eval_batch_size
-            end_idx = (iter + 1) * self.eval_batch_size
+        for iter in range(self.num_batches):
+            print(f"Computing feature activations for batch # {iter+1}/{self.num_batches} in phase # {phase + 1}")
+            start_idx = iter * self.gpt_batch_size
+            end_idx = (iter + 1) * self.gpt_batch_size
             x = X[start_idx:end_idx].to(self.device)
             _, _ = self.transformer(x)
-            mlp_acts_BTF = self.transformer.mlp_activation_hooks[0]
+            mlp_acts = self.transformer.mlp_activation_hooks[0] # (B, T, 4C)
             self.transformer.clear_mlp_activation_hooks()
-            feature_acts_BTH = self.autoencoder.get_feature_activations(inputs=mlp_acts_BTF, 
+            feature_acts = self.autoencoder.get_feature_activations(inputs=mlp_acts, 
                                                                         start_idx=phase*H, 
-                                                                        end_idx=(phase+1)*H)
-            X_PW, feature_acts_PWH = self.select_context_windows(x, feature_acts_BTH, 
+                                                                        end_idx=(phase+1)*H) # (B, T, H)
+            X_PW, feature_acts_PWH = self.select_context_windows(x, feature_acts, 
                                                             num_sampled_tokens=self.num_sampled_tokens, 
                                                             window_radius=self.window_radius, 
                                                             fn_seed=self.seed+iter)
-            idx_start = iter * self.eval_batch_size * self.num_sampled_tokens
-            idx_end = (iter + 1) * self.eval_batch_size * self.num_sampled_tokens
+            idx_start = iter * self.gpt_batch_size * self.num_sampled_tokens
+            idx_end = (iter + 1) * self.gpt_batch_size * self.num_sampled_tokens
             data["tokens"][idx_start:idx_end] = X_PW
             data["feature_acts"][idx_start:idx_end] = feature_acts_PWH
 
         return data
 
     def process_phase(self, data, H, phase):
-        print(f'computing top k feature activations in phase # {phase + 1}/{self.n_phases}')
+        print(f'computing top k feature activations in phase # {phase + 1}/{self.num_phases}')
         _, topk_indices_kH = torch.topk(data["feature_acts"][:, self.window_radius, :], k=self.num_top_activations, dim=0)
         top_acts_data_kWH = TensorDict({
             "tokens": data["tokens"][topk_indices_kH].transpose(dim0=1, dim1=2),
             "feature_acts": torch.stack([data["feature_acts"][topk_indices_kH[:, i], :, i] for i in range(H)], dim=-1)
-        }, batch_size=[self.num_top_activations, 2 * self.window_radius + 1, H])
+        }, batch_size=[self.num_top_activations, self.window_length, H])
 
         for h in range(H):
             self.process_feature(data, H, phase, h, top_acts_data_kWH)
@@ -156,12 +170,12 @@ class FeatureBrowser(ResourceLoader):
         mid_token_feature_acts_M = curr_feature_acts_MW[:, self.window_radius]
         num_nonzero_acts = torch.count_nonzero(mid_token_feature_acts_M)
 
-        feature_id = phase * self.n_features_per_phase + h
+        feature_id = phase * self.num_features_per_phase + h
         if num_nonzero_acts == 0:
             write_dead_feature_page(feature_id=feature_id, dirpath=self.html_out)
             return
         
-        act_density = torch.count_nonzero(curr_feature_acts_MW) / (self.total_sampled_tokens * (2 * self.window_radius + 1)) * 100
+        act_density = torch.count_nonzero(curr_feature_acts_MW) / (self.total_sampled_tokens * self.window_length) * 100
         non_zero_acts = curr_feature_acts_MW[curr_feature_acts_MW != 0]
         make_histogram(activations=non_zero_acts, 
                        density=act_density, 
@@ -188,7 +202,7 @@ class FeatureBrowser(ResourceLoader):
         sampled_acts_data_IXW = TensorDict({
             "tokens": data["tokens"][original_indices],
             "feature_acts": curr_feature_acts_MW[original_indices],
-        }, batch_size=[self.num_intervals, self.samples_per_interval, 2 * self.window_radius + 1])
+        }, batch_size=[self.num_intervals, self.samples_per_interval, self.window_length])
 
         write_alive_feature_page(feature_id=feature_id, 
                                  decode=self.decode,
@@ -256,28 +270,9 @@ class FeatureBrowser(ResourceLoader):
         return result_tensors
 
 if __name__ == "__main__":
-
+    
     torch.manual_seed(seed)
-    feature_browser = FeatureBrowser(
-        dataset=dataset, 
-        gpt_ckpt_dir=gpt_ckpt_dir,
-        device=device,
-        autoencoder_subdir=autoencoder_subdir,
-        num_contexts=num_contexts,
-        num_sampled_tokens=num_sampled_tokens,
-        window_radius=window_radius,
-        eval_batch_size=gpt_batch_size,
-        num_top_activations=num_top_activations,
-        num_intervals=num_intervals,
-        samples_per_interval=samples_per_interval,
-        seed=seed
-    )
-
+    config = FeatureBrowserConfig(**config)
+    feature_browser = FeatureBrowser(config)
     # Run the processing
     feature_browser.build()
-
-
-
-
-
-
